4237c7e5d7e316d684bff70beeb1c35e837b3a2f
joaosantosmedeiros <jopesame@gmail.com>
2023-10-03 15:19:23 -0300

    fix: adjustment in Header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,18 +5,18 @@ export default function Header() {
   return (
     <header>
       <div className="home-btn">
-        <Link to={'/'} id='home'>Home</Link>
+        <Link className='header-link' to={'/'} id='home'>Home</Link>
       </div>
       <div className='navbar'>
         <li>
           <ul>
-            <Link to={'/by-name'} id='search-link'>Pesquisar Receitas <img src="/search.svg" id='search-icon' color='white' width={"22.5px"} /></Link>
+            <Link className='header-link' to={'/by-name'} id='search-link'>Pesquisar Receitas <img src="/search.svg" id='search-icon' color='white' width={"22.5px"} /></Link>
           </ul>
           <ul>
-            <Link to={'/by-letter'} >Receitas por Letra</Link>
+            <Link className='header-link' to={'/by-letter'} >Receitas por Letra</Link>
           </ul>
           <ul>
-            <Link to={'/by-ingredient'}>Receitas por Ingredientes</Link>
+            <Link className='header-link' to={'/by-ingredient'}>Receitas por Ingredientes</Link>
           </ul>
         </li>
       </div>

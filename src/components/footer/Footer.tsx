053154ef7af12ba862a07f029873164d78588152
joaosantosmedeiros <jopesame@gmail.com>
2023-10-06 10:45:02 -0300

    fix: change Footer component <a> tags to <Link>

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import './Footer.css'
 
 export default function Footer() {
   return (
     <footer>
       <div className="social-media">
-        <a href="https://www.facebook.com"><img src="/facebook.svg" height={"30px"} alt="" /></a>
-        <a href="https://www.instagram.com"><img src="/instagram.svg" height={"30px"} alt="" /></a>
-        <a href="https://web.whatsapp.com"><img src="/whatsapp.svg" height={"30px"} alt="" /></a>
+        <Link to="https://www.facebook.com"><img src="/facebook.svg" data-testid="facebook" height={"30px"} alt="" /></Link>
+        <Link to="https://www.instagram.com"><img src="/instagram.svg" data-testid="whatsapp" height={"30px"} alt="" /></Link>
+        <Link to="https://web.whatsapp.com"><img src="/whatsapp.svg" data-testid="instagram" height={"30px"} alt="" /></Link>
       </div>
       <p>Copyright © 2023 ChefeEmCasa inc. All rights Reserved</p>
     </footer>

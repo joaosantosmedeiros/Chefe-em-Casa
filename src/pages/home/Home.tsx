45127f759e02b077af8978c519fa526e13f99ea1
joaosantosmedeiros <jopesame@gmail.com>
2023-10-06 10:45:44 -0300

    fix: change number of api calls from 5 to 10 in Home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -15,7 +15,7 @@ function Home() {
       setMealList((prevMealList) => [...prevMealList, newMeal]);
     };
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 10; i++) {
       fetchRandomMeal();
     }
   }, []);
